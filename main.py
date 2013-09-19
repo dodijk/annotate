fd359add8721a59444e38271cd96959975bcdcd3
Daan Odijk <d.odijk@uva.nl>
2013-09-19 19:52:03 +0200

    Prototype

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/prototype.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
